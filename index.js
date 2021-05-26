7e32df9f800612a971767a1c9856a6a635001218
b45i <amalshajan2011@gmail.com>
2021-05-26 19:32:01 +0530

    cache implementaion

--- index.js
+++ index.js
@@ -4,22 +4,33 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors());
-app.options('*', cors());;
+app.options('*', cors());
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 TorrentSearchApi.enablePublicProviders();
+
+const cache = {};
 
 app.get('/', (req, res) => {
     res.send('ഇവിടെ ഒരു മൈരും ഇല്ല !');
 });
 
 app.get('/search/:text', async (req, res) => {
+    console.log('Search:', req.params.text);
+
+    if (cache[req.params.text.toLowerCase()]) {
+        console.log('Sending cached data...');
+        return res.json(cache[req.params.text]);
+    }
+
     try {
         const torrents = await TorrentSearchApi.search(req.params.text);
-        res.json({
+        const result = {
             torrents,
             count: torrents.length,
-        });
+        };
+        cache[req.params.text.toLowerCase()] = result;
+        return res.json(result);
     } catch (err) {
         console.error(err.message);
         res.status(500).send('പൂണ്ട  മൈര്, സെർവർ  ഊമ്പി ');
